18c3779ad77fa9632d8d9c44d12a1b3191547389
ThRangel <the.beatles.lma2@gmail.com>
2019-12-28 15:20:08 -0600

    Terminado

--- ZoomTH/ViewController.swift
+++ ZoomTH/ViewController.swift
@@ -26,5 +26,30 @@ extension ViewController : UIScrollViewDelegate{
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return miImageView
     }// del la func viewForZooming
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        if miScroll.zoomScale > 1{
+            if let imagen = miImageView.image{//segundo if
+                let radioAncho = miImageView.frame.width / imagen.size.width
+                let radioLargo = miImageView.frame.height / imagen.size.height
+                
+                let radio = radioAncho < radioLargo ? radioAncho : radioLargo
+                let nuevaAnchura = imagen.size.width * radio
+                let nuevoLargo = imagen.size.height * radio
+                
+                let conditionLeft = nuevaAnchura*miScroll.zoomScale>miImageView.frame.width
+                let left = 0.5 * (conditionLeft ? nuevaAnchura - miImageView.frame.width : (miScroll.frame.width - miScroll.contentSize.width) )
+                
+                let condicionArriba = nuevoLargo*miScroll.zoomScale > miImageView.frame.height
+                let arriba = 0.5 * (condicionArriba  ? nuevoLargo - miImageView.frame.height : (miScroll.frame.height - miScroll.contentSize.height) )
+                miScroll.contentInset = UIEdgeInsets(top: arriba, left: left, bottom: arriba, right: left)
+                
+            }//del segundo if
+            else{
+                miScroll.contentInset = .zero
+            }
+        }//del primer if
+    }//de la funcion scrollViewDidZoom
+    
+    
 }//del extension
 
